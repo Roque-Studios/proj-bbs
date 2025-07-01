1bf3c7fe08652df826a4aa02dd80d756b4ebcb68
Kevin Rivera <kevinrev26@gmail.com>
2025-06-30 18:26:12 -0600

    [86b5hf0bc] MOD1: Base scene, fixed cam and top-down view

---
+++ main/Stadium/Stadium.go
@@ -0,0 +1,18 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"Green_Nebula_08-1024x1024\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/Stadium/Stadium.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 1027.0
+    y: 513.0
+  }
+  scale {
+    x: 2.0
+  }
+}
